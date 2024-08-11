15e79c7aec42e81e97d16b853d089e34022fd3a2
trader18512 <augustinemwangi54@gmail.com>
2024-08-11 07:39:48 +0300

    first commit

---
+++ src/dfinity_js_frontend/src/components/Chat.js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import useApi from "../hooks/useApi";
+import Loading from "./Loading";
+import { useEffect } from "react";
+import { login, logout } from "../utils/auth";
+import toast from "react-hot-toast";
+import { getConversation } from "../utils/chat";
+import TextInput from "./TextInput";
+import { encryptData } from "../utils/encryptData";
+
+export default function Chat() {
+  const [question, setQuestion] = useState("");
+  const [openaiKey, setOpenaiKey] = useState("");
+  const { loading, chatCompletion, chatMessage, setChatMessage } = useApi();
+
+  const updateChatMessage = async () => {
+    if (window.auth.principalText && window.auth.isAuthenticated) {
+      const conversation = await getConversation(window.auth.principalText);
+      console.log(conversation);
+      if (conversation) {
+        setChatMessage(conversation.conversation);
+      }
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!window.auth.isAuthenticated) {
+      toast.error("You are not authenticated");
+      return;
+    }
+
+    const openaiKey = localStorage.getItem("icp-dai-open-ai");
+    if (!openaiKey) {
+      toast.error("No openai key found");
+      return;
+    }
+
+    if (question) {
+      const history = [...chatMessage, { content: question, role: "user" }];
+      setChatMessage(() => [...history]);
+      await chatCompletion(history);
+      setQuestion("");
+    }
+  };
+
+  useEffect(() => {
+    updateChatMessage();
+  }, []);
+
+  const onValidateOpenaiAPI = (e) => {
+    if (e.target.value.match(/^sk-proj-[a-zA-Z0-9]+$/)) {
+      setOpenaiKey(e.target.value);
+    } else {
+      setOpenaiKey("");
+    }
+  };
+
+  const onSaveOpenaiKey = () => {
+    // if (!openaiKey) return toast.error("Invalid Openai key");
+    const encryptedApiKey = encryptData(openaiKey);
+    localStorage.setItem("icp-dai-open-ai", encryptedApiKey);
+    toast.success("Openai key successfully saved and crypted");
+    setOpenaiKey("");
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="wrapper-header">
+        <h1>Dai</h1>
+        <button
+          className="auth-button auth-button__hover"
+          onClick={() => (window.auth.isAuthenticated ? logout() : login())}
+        >
+          {window.auth.isAuthenticated ? "Log out" : "Login"}
+        </button>
+      </div>
+      <div style={{ display: "flex", gap: 10 }}>
+        <TextInput
+          onChange={onValidateOpenaiAPI}
+          placeholder="Pass your Openai API key here..."
+        />
+        <button
+          className="auth-button auth-button__hover"
+          onClick={onSaveOpenaiKey}
+        >
+          Save
+        </button>
+      </div>
+      <div className="container">
+        <div className="right">
+          <div className="chat active-chat">
+            <div className="conversation-start"></div>
+            {chatMessage.map((message, index) => (
+              <div
+                key={index}
+                className={`bubble ${
+                  message.role === "user" ? "me" : "assistant"
+                } ${
+                  chatMessage.length - 1 === index && !loading
+                    ? "last-message"
+                    : ""
+                }
+                `}
+              >
+                {message.content}
+              </div>
+            ))}
+
+            {loading && (
+              <div className={`bubble assistant`}>
+                <Loading />
+              </div>
+            )}
+          </div>
+          <div className="write">
+            <input
+              placeholder="Ask me..."
+              type="text"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              onKeyDown={(e) => (e.key === "Enter" ? handleSubmit(e) : null)}
+            />
+            {loading && <Loading />}
+            {!loading && (
+              <a
+                onClick={(e) => {
+                  handleSubmit(e);
+                }}
+                className="write-link send"
+              ></a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
